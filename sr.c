cea68f27cbb7d851990ab094c077b0e04e34c09f
Jesse-11 <jesse.hopppo@gmail.com>
2025-05-01 14:52:27 +0930

    updated a_init

--- sr.c
+++ sr.c
@@ -199,14 +199,12 @@ void A_timerinterrupt(void)
 /* entity A routines are called. You can use it to do any initialization */
 void A_init(void)
 {
-  /* initialise A's window, buffer and sequence number */
-  A_nextseqnum = 0;  /* A starts with seq num 0, do not change this */
-  windowfirst = 0;
-  windowlast = -1;   /* windowlast is where the last packet sent is stored.
-		     new packets are placed in winlast + 1
-		     so initially this is set to -1
-		   */
-  windowcount = 0;
+  base_a = 0;
+  next_a = 0;
+  _timer_running = false;
+  for (int i = 0; i < SEQSPACE; i++) {
+    ack_send[i] = false;
+  }
 }
 
 
